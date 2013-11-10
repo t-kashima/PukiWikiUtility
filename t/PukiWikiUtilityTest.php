c3bc36f65ae24744bf74767c6f0459cbe534dbef
dekokun <shintaro.kurachi@gmail.com>
2013-11-10 09:30:35 +0900

    assertで失敗したらテスト実行が終了するようにし、成功したらtest okを出力

--- t/PukiWikiUtilityTest.php
+++ t/PukiWikiUtilityTest.php
@@ -1,5 +1,6 @@
 <?php
 require_once(dirname(__DIR__) . '/PukiWikiUtility.class.php');
+assert_options(ASSERT_BAIL, 1);
 //$result = PukiWikiUtility::search('http://pukiwiki.sourceforge.jp/', 'PukiWiki');
 $result = array(  2513 =>
                   array (
@@ -11,7 +12,9 @@ $result = array(  2513 =>
                     'title' => '２ちゃんねる・ひろゆき敗訴の影響について',
                     'url' => 'http://pukiwiki.sourceforge.jp/?cmd=read&amp;page=%EF%BC%92%E3%81%A1%E3%82%83%E3%82%93%E3%81%AD%E3%82%8B%E3%83%BB%E3%81%B2%E3%82%8D%E3%82%86%E3%81%8D%E6%95%97%E8%A8%B4%E3%81%AE%E5%BD%B1%E9%9F%BF%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6&amp;word=PukiWiki',
                   ),);
-assert(isset($result));
+assert(!isset($result), 'hogehoge');
+
+echo 'test ok' . PHP_EOL;
 
 
 
