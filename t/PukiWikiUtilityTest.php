585c046b73fd12fd4542202a12cc6c9f4a1f4c44
dekokun <shintaro.kurachi@gmail.com>
2013-11-10 10:03:25 +0900

    変なものまでコミットしていたので戻す

--- t/PukiWikiUtilityTest.php
+++ t/PukiWikiUtilityTest.php
@@ -1,17 +1,8 @@
 <?php
 require_once(dirname(__DIR__) . '/PukiWikiUtility.class.php');
 assert_options(ASSERT_BAIL, 1);
-//$result = PukiWikiUtility::search('http://pukiwiki.sourceforge.jp/', 'PukiWiki');
-$results = array(  2513 =>
-                  array (
-                    'title' => '龍司',
-                    'url' => 'http://pukiwiki.sourceforge.jp/?cmd=read&amp;page=%E9%BE%8D%E5%8F%B8&amp;word=PukiWiki',
-                  ),
-                  2514 =>
-                  array (
-                    'title' => '２ちゃんねる・ひろゆき敗訴の影響について',
-                    'url' => 'http://pukiwiki.sourceforge.jp/?cmd=read&amp;page=%EF%BC%92%E3%81%A1%E3%82%83%E3%82%93%E3%81%AD%E3%82%8B%E3%83%BB%E3%81%B2%E3%82%8D%E3%82%86%E3%81%8D%E6%95%97%E8%A8%B4%E3%81%AE%E5%BD%B1%E9%9F%BF%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6&amp;word=PukiWiki',
-                  ),);
+$result = PukiWikiUtility::search('http://pukiwiki.sourceforge.jp/', 'PukiWiki');
+
 assert(isset($results));
 assert(is_array($results));
 foreach($results as $result) {
